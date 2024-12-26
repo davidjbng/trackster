461f466600adbd39978ab65014fcae3e0d71d35c
David Jebing <29679909+davidjbng@users.noreply.github.com>
2024-12-26 10:59:58 +0100

    init web page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import { Form } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Trackster" },
+    { name: "description", content: "Create your own song guessing game" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="h-full">
+      <div className="grid place-items-center h-full">
+        <h1 className="text-5xl">Welcome to Trackster</h1>
+        <Form className="flex flex-col self-start gap-3">
+          <label htmlFor="playlistLink">
+            Enter a link to your Spotify playlist
+          </label>
+          <input
+            id="playlistLink"
+            type="text"
+            placeholder="Your playlist"
+            name="playlistLink"
+            className="rounded-md px-4 py-3"
+          />
+          <button
+            type="submit"
+            className="bg-green-700 rounded-lg px-4 py-3 mt-4 hover:bg-green-800"
+          >
+            Download QR Codes
+          </button>
+        </Form>
+      </div>
+    </main>
+  );
 }
