8681350b3993f5819a50d00f8cf7ec3c78773f2d
David Jebing <29679909+davidjbng@users.noreply.github.com>
2024-12-26 11:02:59 +0100

    init action

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,12 +8,18 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+export function action({ params, request }: Route.ActionArgs) {
+  const playlistLink = new URL(request.url).searchParams.get("playlistLink");
+  console.log(playlistLink);
+  return null;
+}
+
 export default function Home() {
   return (
     <main className="h-full">
       <div className="grid place-items-center h-full">
         <h1 className="text-5xl">Welcome to Trackster</h1>
-        <Form className="flex flex-col self-start gap-3">
+        <Form className="flex flex-col self-start gap-3" method="post">
           <label htmlFor="playlistLink">
             Enter a link to your Spotify playlist
           </label>
@@ -23,6 +29,7 @@ export default function Home() {
             placeholder="Your playlist"
             name="playlistLink"
             className="rounded-md px-4 py-3"
+            required
           />
           <button
             type="submit"
